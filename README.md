1fd900a72aaf2b63cdc6065dab3810a1b6459724
kyleknap <kyknapp1@gmail.com>
2021-11-29 17:36:38 -0800

    Initial main branch implementation

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
-## My Project
+# aws-cli-workshop
+This repository hosts the code needed to work through the
+AWS CLI workshop.
 
-TODO: Fill this README out!
+## Repository layout
 
-Be sure to:
+The repository consists of the following layout:
 
-* Change the title in this README
-* Edit your repository description on GitHub
+* `scripts/` - This directory contains any of the scripts that will
+   be used in the workshop.
+* `infra/` - This directory contains any of the core CloudFormation
+   templates used in the workshop.
+* `assets/` - This directory contains any additional files that to be
+   included in the repository (e.g. AWS CLI wizard snippets and sample
+   EventBridge patterns)
+* `Dockerfile` - Allows us to build an image of this repository for easy
+   access to its scripts and any additional tooling we may want.
+* `buildspec.yml` - CodeBuild build specification to build the repository
+   docker image and deploy it to an Amazon ECR repository.
 
 ## Security
 
---
+++ scripts/run-build.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+usage() {
+  cat 1>&2 <<EOF
+Runs and displays progress of a CodeBuild project build
+USAGE:
+    run-build.sh [-h|--help] <project-name> [FLAGS] [OPTIONS]
+
+This script accepts all valid parameters for aws codebuild start-build command.
+If --debug-session-enabled is set, it will automatically open a session to
+the in-progress build.
+EOF
+}
+
+
+parse_commandline() {
+  case "$1" in
+    -h|--help)
+	  usage
+      exit 0
+	;;
+  esac
+  if [ -z "$1" ]; then
+    echo "Must provide the <project-name> argument." 1>&2
+    exit 1
+  fi
+  PROJECT_NAME="$1"
+  EXTRA_START_ARGS="${*:2}"
+}
+
+
+start_codebuild() {
+  # TODO: Implement call to aws codebuild start-build
+  # build_id=$(aws codebuild start-build ... )
+  echo "Started build: $build_id"
+}
+
+wait_for_build_phase() {
+  expected_status="$1"
+  # TODO: Implement the call to batch-get-builds and extract the currentPhase
+  #       value from the output using `--query`
+  until [ $(aws codebuild batch-get-builds ...) == "$expected_status" ]
+  do
+    sleep 1
+  done
+}
+
+tail_build() {
+  build_logs=$(aws codebuild batch-get-builds --ids "$build_id" --query 'builds[].logs.[groupName,streamName]' --output text)
+  log_name=$(echo "$build_logs" | cut -f 1)
+  stream_name=$(echo "$build_logs" | cut -f 2)
+  aws logs tail "$log_name" --log-stream-names "$stream_name" --follow --format short
+}
+
+connect_ssm() {
+  session_target=$(aws codebuild batch-get-builds --ids $build_id --query 'builds[].debugSession[].sessionTarget' --output text)
+  until [[ ! -z "$session_target" ]]
+  do
+    sleep 1
+    session_target=$(aws codebuild batch-get-builds --ids $build_id --query 'builds[].debugSession[].sessionTarget' --output text)
+  done
+  sleep 15  # Wait for logs to get propagated before starting session
+  aws ssm start-session --target "$session_target"
+}
+
+main() {
+  parse_commandline "$@"
+  start_codebuild
+  wait_for_build_phase PROVISIONING
+  tail_build &
+  if [[ "$*" == *--debug-session-enabled* ]]
+  then
+    connect_ssm
+  fi
+  wait_for_build_phase COMPLETED
+  sleep 15  # Wait for logs to get propagated
+  kill "$!"
+  exit 0
+}
+
+main "$@" || exit 1
